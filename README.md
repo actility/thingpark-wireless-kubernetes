56540110ef31e47d26afce361910ff157dcc3546
Jordane Fillatre <jordane.fillatre@actility.com>
2022-12-01 14:29:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Thingpark deployment on kubernetes
+
+- Installation and administration guides are published on [ThingPark Documentation Portal](https://docs.preprod.thingpark.com/thingpark-wireless-kubernetes-guide/7.2/) preprod
+
+## Project structure
+- `configs/<distribution>`: provide values to configure deployment on supported kubernetes distributions
+- `samples/`: values files to getting start a new deployment
---
+++ configs/build-configs.sh
@@ -0,0 +1,31 @@
+# Azure profiles
+flavors=(azure amazon)
+segment=(l)
+unsupported=()
+
+for f in "${flavors[@]}"
+do
+    echo "Generate $f configs"
+    rm -vr ${f}
+    mkdir -vp ${f}
+    for s in "${segment[@]}"
+    do
+        if [ -f  reference/${f}/values-${s}-segment.yaml ]; then
+        cloud_values="reference/${f}/values-${s}-segment.yaml"
+        else 
+        cloud_values="reference/${f}/values.yaml"
+        fi
+        echo "Generate $s configs for $f"
+            yq eval-all '. as $item ireduce ({}; . * $item)' reference/values-selectors.yaml \
+                reference/compute/values-${s}-segment.yaml \
+                reference/storage/values-${s}-segment.yaml \
+                ${cloud_values} | tee ${f}/values-${s}-segment.yaml
+    done
+done
+ 
+for item in "${unsupported[@]}"
+do
+    if [ -f ${item} ]; then
+        rm -v ${item}
+    fi
+done
